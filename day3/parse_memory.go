8be547fa5cfa61c75ba0a362fd51dacd1bee9793
bpsoos <soos.peter.96@gmail.com>
2024-12-03 23:12:19 +0100

    adds day 3 solution 2

--- day3/parse_memory.go
+++ day3/parse_memory.go
@@ -10,16 +10,52 @@ import (
 type Mul [2]int
 
 func ParseMemory(rawMemory io.Reader) []Mul {
-	muls := make([]Mul, 0)
 	memorySectionBytes, err := io.ReadAll(rawMemory)
 	if err != nil {
 		panic(err)
 	}
-	remaining := string(bytes.TrimSpace(memorySectionBytes))
+
+	return getMuls(string(bytes.TrimSpace(memorySectionBytes)))
+}
+
+func ParseMemoryWithDosAndDonts(rawMemory io.Reader) []Mul {
+	memorySectionBytes, err := io.ReadAll(rawMemory)
+	if err != nil {
+		panic(err)
+	}
+	unstripped := string(bytes.TrimSpace(memorySectionBytes))
+	stripped := stripDonts(unstripped)
+
+	return getMuls(stripped)
+}
+
+func stripDonts(rawMemory string) string {
+	stripped := ""
+	remaining := rawMemory
+	for {
+		before, after, contains := strings.Cut(remaining, "don't()")
+		if !contains {
+			stripped += remaining
+			break
+		}
+		stripped += before
+		_, after, contains = strings.Cut(after, "do()")
+		if !contains {
+			break
+		}
+		remaining = after
+	}
+
+	return stripped
+}
+
+func getMuls(rawMemory string) []Mul {
+	muls := make([]Mul, 0)
 	var (
 		contains                  bool
 		num1String, num2String, r string
 	)
+	remaining := rawMemory
 	for {
 		_, remaining, contains = strings.Cut(remaining, "mul(")
 		if !contains {
--- day3/solver.go
+++ day3/solver.go
@@ -21,10 +21,15 @@ func (solver Solver) Solve() {
 	fmt.Println(calculate(ParseMemory(bytes.NewReader(testInput))))
 	fmt.Println("day 3 solution 1:")
 	fmt.Println(calculate(ParseMemory(bytes.NewReader(input))))
+
+	test2Input, err := os.ReadFile("inputs/day3_test2.txt")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("day 3 test 2:")
-	fmt.Println()
+	fmt.Println(calculate(ParseMemoryWithDosAndDonts(bytes.NewReader(test2Input))))
 	fmt.Println("day 3 solution 2:")
-	fmt.Println()
+	fmt.Println(calculate(ParseMemoryWithDosAndDonts(bytes.NewReader(input))))
 }
 
 func calculate(muls []Mul) int {
